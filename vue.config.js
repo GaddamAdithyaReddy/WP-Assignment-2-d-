facee37889e2a3b3e2be8d3bb348b852b72fae4e
GaddamAdithyaReddy <g.adithyareddy0810@gmail.com>
2026-05-05 18:06:37 +0530

    Update repository name for GitHub Pages: WP-Assignment-2-d-

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/WP-Assignment-2/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/WP-Assignment-2-d-/' : '/'
 })
